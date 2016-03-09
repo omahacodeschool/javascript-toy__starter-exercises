fdef869cc87879b05cfa387224adba6819f4b633
Tray Harrington <traylon.harrington@gmail.com>
2016-03-09 12:16:42 -0600

    04 Done

---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,14 @@
+var name = prompt("What is your name?")
+var age = prompt("What is your age?")
+var hometown = prompt("What is your hometown?")
+var favNumber = prompt("What is your favorite number?")
+
+alert("Your name is " + name + ".")
+alert("You are " + age + " years old.")
+alert("You were born in " + hometown + ".")
+alert("Your favorite number is " + favNumber + ".")
+
+console.log("Your name is " + name + ".")
+console.log("You are " + age + " years old.")
+console.log("You were born in " + hometown + ".")
+console.log("Your favorite number is " + favNumber + ".")
