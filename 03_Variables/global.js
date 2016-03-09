bb4873d921af6fa544755ab4bd1daacc59faeae0
Tyler <tfisher322@gmail.com>
2016-03-09 10:56:25 -0600

    First three complete

---
+++ 03_Variables/global.js
@@ -0,0 +1,9 @@
+ var name = "Tyler";
+
+ var age = 25;
+
+ var hairColor = "blonde";
+
+ alert("My name is" + " " + name);
+ alert("My age is" + " " + age);
+ alert("My hair color is" + " " + hairColor);
