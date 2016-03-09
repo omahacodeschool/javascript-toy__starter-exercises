d399b37d8782ac7465aa5d74cdf688e0ff6c9885
Kyle Sandage <ksandage08@gmail.com>
2016-03-09 14:03:01 -0600

    Doing math

---
+++ 05_BasicMath/global.js
@@ -0,0 +1,27 @@
+// Get numbers
+var first_num = prompt("Give me a number.");
+var second_num = prompt("Give me a number.");
+var third_num = prompt("Give me a number.");
+
+// Do math
+var sum = first_num + second_num + third_num
+var differenc = first_num - second_num - third_num
+var product = first_num * second_num * third_num
+var quotient = first_num / second_num
+var increase = first_num++
+var decrease = second_num--
+
+// Educate user
+alert("The sum of " + first_num "," + second_num + ", and" + third_num + "is " + sum +".")
+alert("The difference of " + first_num + " from" + second_num " and" + third_num + "is " + difference +".")
+alert("The product of " + first_num + " ," + second_num + ", and" + third_num + "is " + product +".")
+alert("The quotient of " + first_num + " divided by" + second_num +
+ "is " + quotient + ".")
+alert(first_num + " increased by 1 is " + increase + ".")
+alert(first_num + " decreased by 1 is " + decrease + ".")
+
+// Log Stuff
+console.log(first_num)
+console.log(second_num)
+console.log(third_num)
+console.log(sum)
