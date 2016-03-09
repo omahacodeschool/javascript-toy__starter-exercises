409f8ce8330f84822cfa3b7d203886aeb90dba84
Chris Foral <chris.foral@gmail.com>
2016-03-09 15:43:23 -0600

    Atomic 2 complete, I believe.

---
+++ 02_OutputWithAlertAndLog/global.js
@@ -0,0 +1,16 @@
+// Greeting alert
+alert("Hello! Chris here. Welcome to this thing.");
+
+// Favorite breakfast
+console.log("My favorite breakfast has to be eggs benedict.");
+// Cat count
+console.log("I live with too few cats with the count being but one.");
+// Favorite eats
+console.log("My favorite place to gorge myself is España in Benson.");
+// Nap count
+console.log("Naps have been a point of contention in my life and my current weekly nap count stands at 0");
+// Desired nap count
+console.log("If we could add more hours to a day or if I could freeze time, I would make naps a daily requirement.");
+
+// Goodbye alert
+alert("Goodbye for now!");
