3bc29f725fc8aa8c07c71ca1d6e2983ec9abb3c1
Tray Harrington <traylon.harrington@gmail.com>
2016-03-09 10:46:37 -0600

    02 Done

---
+++ 02_OutputWithAlertAndLog/global.js
@@ -0,0 +1,12 @@
+// Greeting
+alert("Hello stranger! My name is Shietokie Mietokie!");
+
+//Console Statments
+console.log("My favorite breakfast is a scrambled egg sandwich with bacon!");
+console.log("Unfortunately I do not have any cats. I do however have a pet Coeurl minion!");
+console.log("My favorite place to eat lots of food is the Bismarck in Limsa Lominsa!");
+console.log("The number of naps I take a week is probably close to 7. I average about a nap a day");
+console.log("The number of naps I would like to take per week is ZERO! I wish I didn't have to sleep and could spend my entire day adventuring!");
+
+//Closing
+alert("Thanks for chatting with me! Goodbye!");
