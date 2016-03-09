ec19612df920c2e4696a6bdeeed553d43df7fdf9
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-09 12:24:29 -0600

    Atomic 02 done

--- 02_OutputWithAlertAndLog/global.js
+++ 02_OutputWithAlertAndLog/global.js
@@ -4,7 +4,7 @@
 alert("Hi, my name is Jillian.");
 
 //console.log statements:
-console.log("My favorite breakfast is no breakfast. Eating breakfast just makes me hungry like five minutes later.");
+console.log("My favorite breakfast is no breakfast. Eating breakfast just makes me hungry, like, five minutes later.");
 console.log("I live with two large, interesting cats. We have been roommates for ten years.");
 console.log("My favorite place to eat lots of food is at home. That way I know what's in it.");
 console.log("I take zero naps per week unless I'm sick.");
