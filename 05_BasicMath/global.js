093d65cb7a0e7081b9b591c7d076c0277248f2b1
Kyle Sandage <ksandage08@gmail.com>
2016-03-09 14:45:52 -0600

    Math works

--- 05_BasicMath/global.js
+++ 05_BasicMath/global.js
@@ -10,23 +10,29 @@ third_num = parseInt(third_num)
 
 // Do math
 var sum = (first_num + second_num + third_num);
-var differenc = (first_num - second_num - third_num);
+var difference = (first_num - second_num - third_num);
 var product = (first_num * second_num * third_num);
 var quotient = (first_num / second_num);
-var increase = (first_num++);
-var decrease = (second_num--);
+
 
 // Educate user
-alert( "The sum of " + first_num "," + second_num + ", and" + third_num + "is " sum +".");
-alert("The difference of " + first_num + " from" + second_num " and" + third_num + "is " + difference +".");
-alert("The product of " + first_num + " ," + second_num + ", and" + third_num + "is " + product +".");
-alert("The quotient of " + first_num + " divided by" + second_num +
- "is " + quotient + ".");
-alert(first_num + " increased by 1 is " + increase + ".");
-alert(first_num + " decreased by 1 is " + decrease + ".");
+alert( "The sum of " + first_num + ", " + second_num + ", and " + third_num
+  + " is " + sum +".");
+alert("The difference of " + first_num + " from " + second_num + " and " + third_num + " is " + difference + ".");
+alert("The product of " + first_num + ", " + second_num + ", and " + third_num + " is " + product +".");
+alert("The quotient of " + first_num + " divided by " + second_num +
+ " is " + quotient + ".");
+alert(first_num + " increased by 1 is... ");
+first_num++;
+alert(first_num)
+alert(second_num + " decreased by 1 is... ");
+second_num--
+alert(second_num)
 
 // Log Stuff
-console.log(first_num);
-console.log(second_num);
-console.log(third_num);
-console.log(sum);
+console.log("first_num is " + first_num);
+console.log("second_num is " + second_num);
+console.log("third_num is " + third_num);
+console.log("sum is " + sum);
+console.log("difference is " + difference);
+console.log("quotient is " + quotient);
