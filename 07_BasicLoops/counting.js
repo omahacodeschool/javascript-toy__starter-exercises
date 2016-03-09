46385201f4ccd76f82114deeab13dbffbc987d5d
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-09 16:49:29 -0600

    wip counting.js

--- 07_BasicLoops/counting.js
+++ 07_BasicLoops/counting.js
@@ -3,12 +3,16 @@
 var count = "";
 var start = prompt("Give me a starting value", "enter number");
 var end = prompt("Now give me an ending value", "enter number");
-var increment = prompt("What increment would you like to count by", "enter number");
+var increment = prompt("What increment would you like to count by?", "enter number");
+
+start_at = parseInt(start);
+end_at = parseInt(end);
+increment_by = parseInt(increment);
 
 
-while (start < end) {
-    alert(count += start + ",");
-    start + increment;
+while (start_at < end_at) {
+    alert(count += start_at + ",");
+    start_at + increment_by;
 }
 
 
