89a2280a981e5b81b3fd0fe25de1d353d743149d
Tyler <tfisher322@gmail.com>
2016-03-09 13:12:18 -0600

    Password portion of the task is done.

---
+++ 06_IfStatements/global.js
@@ -0,0 +1,16 @@
+console.log( "5 == '5' is " + (5 == '5') ); // true
+console.log( "5 == 'five' is " + (5 == 'five') ); // false
+console.log( "5 == '6' is " + (5 == '6') ); // false
+console.log( "5 === '5' is " + (5 === '5') ); // false
+console.log( "5 == 2 + 3 is " + (5 == 2 + 3) ); // true
+console.log( "5 == 5 is " + (5 == 5) ); // true
+console.log( "5 === 5 is " + (5 === 5) ); // true 
+console.log( "true == 'true' is " + (true == 'true') ); // false
+console.log( "true === 'true' is " + (true === 'true') ); // false
+console.log( "true == false is " + (true == false) ); // false
+console.log( "5 == 12 is " + (5 == 12) ); // false
+console.log( "5 != 12 is " + (5 != 12) ); // true
+console.log( "5 < 12 is " + (5 < 12) ); // true
+console.log( "5 <= 12 is " + (5 <= 12) ); // false. Wrong. I was a bit mixed on my answer. I decided last second to say false due to the '=' there.
+console.log( "5 > 12 is " + (5 > 12)); // false
+console.log( "5 >= 12 is " + (5 >= 12)); // false
---
+++ 06_IfStatements/password.js
@@ -0,0 +1,10 @@
+var password = "12345";
+var inputPass = prompt("Please enter your password");
+
+var passVerification;
+if (inputPass == password) {
+    alert("That's correct!");
+}
+else {
+    alert("That's incorrect!") ;
+}
