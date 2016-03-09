ff245e376680d2325e83d2fdd8059168575bddc6
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-09 17:18:59 -0600

    console.log messages are in the global.js file, but not showing up in the UX

--- 02_OutputWithAlertAndLog/global.js
+++ 02_OutputWithAlertAndLog/global.js
@@ -1,3 +1,11 @@
-alert("Whoa there Grizzly Bear");
+// alert message when a user first loads the page.
+alert("Hello, my name is Brad");
 
-console.log("Where's your invite?");
+// console.log statements
+console.log("My favorite breakfast is Croche Madam.");
+console.log("One cat resides at my house.");
+console.log("My favorite place to eat lots of food is Dario's.");
+console.log("I take at least 5 naps per week.");
+
+// alert message for when the user has viewed the console log.
+alert("Now that that's out of the way...");
