31204b5840d2719039491bf996c969c9f889cbc6
Chris Foral <chris.foral@gmail.com>
2016-03-09 16:43:32 -0600

    Atomic 03 done

--- 03_Variables/global.js
+++ 03_Variables/global.js
@@ -1,3 +1,17 @@
-var name       = "Chris"
-var age        = "29"
-var hairColor = "Blond-ish"
+// Creating the variables
+var name       = "Chris";
+var age        = 29;
+var hairColor  = "Blond-ish";
+var undefinedVariable = 
+
+// Creating the alerts with the values of the variables
+alert("My name is " + name);
+alert("My age is " + age);
+alert("My hair color is " + hairColor);
+alert("Undefined variable test: " + undefinedVariable);
+alert("Absent variable test: " + absentVariable);
+
+// Console logs
+console.log("My name is " + name);
+console.log("My age is " + age);
+console.log("My hair color is " + hairColor);
