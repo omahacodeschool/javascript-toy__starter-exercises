b2d6271a22384183cc9ea271ddcf82dce8e2d787
Tyler <tfisher322@gmail.com>
2016-03-09 16:00:59 -0600

    working on Fizz function.

--- 07_BasicLoops/counting.js
+++ 07_BasicLoops/counting.js
@@ -1,5 +1,31 @@
-var i = 0
-while (i <= 10) {
-    console.log( "The number is " + i); 
+var start = prompt("What number do you want to start at?");
+start = parseInt(start);
+
+var end = prompt("What number do you want to end at?");
+end = parseInt(end);
+
+var i = start
+
+
+while (i <= 1) {
+  console.log( i + " " + "bottle of beer on the wall" + " " + i + " " + "bottle of beer. Take one down, pass it around." + " " + i + " " + "bottle of beer on the wall."); 
+  i++;
+
+}
+while (i < end && i > 1) {
+  console.log( i + " " + "bottles of beer on the wall" + " " + i + " " + "bottles of beer. Take one down, pass it around." + " " + i + " " + "bottles of beer on the wall."); 
     i++;
+}
+
+while
+  (i >= end) {
+  console.log(i + " " + "bottles of beer on the wall" + " " + i + " " + "bottles of beer. Take one down, pass it around." + " " + i + " " + "bottles of beer on the wall."); 
+    i--;
+} 
+while (i <= 1) {
+  console.log( i + " " + "bottle of beer on the wall" + " " + i + " " + "bottle of beer. Take one down, pass it around." + " " + i + " " + "bottle of beer on the wall."); 
+}
+
+while (i / 3) {
+  console.log ("fizz")
 }
