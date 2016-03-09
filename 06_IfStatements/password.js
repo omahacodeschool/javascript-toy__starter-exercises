b109f2cc70c9eb82b0b8a17d4c2a6d48cc29d969
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-09 13:51:53 -0600

    working on atomic 6

---
+++ 06_IfStatements/password.js
@@ -0,0 +1,11 @@
+//6B: Password Game
+
+var password = "Nachos";
+
+var userPassword = prompt("Password please.", "Enter password here.");
+
+if (password == userPassword) {
+  alert("Good job!");
+} else {
+  alert("Sorry, the password is actually Nachos.");
+}
