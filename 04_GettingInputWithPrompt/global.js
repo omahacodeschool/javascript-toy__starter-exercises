d49507009a88fe97167bedefdedc08d030b8e709
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-09 11:14:16 -0600

    Output answers

---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,17 @@
+
+var name = prompt("What is your name?", "Type your name here.");
+var hometown = prompt("What is your hometown?", "Type the town here.");
+var number = prompt("What is your favorite number?", "Type the number here.");
+
+
+//Console.log statment:
+console.log("Hi" + " " + name);
+console.log(hometown + " " + "is such a beautiful place.");
+console.log(number + " " + "is the luckiest number of them all.");
+
+//Alerts
+alert("Hi" + " " + name);
+alert(hometown + " " + "is such a beautiful place.");
+alert(number + " " + "is the luckiest number of them all.");
+
+
