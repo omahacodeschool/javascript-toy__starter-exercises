9699ed5d0111e98daa5d5273b9bb81f6eaedcf0d
Chris Foral <chris.foral@gmail.com>
2016-03-09 17:21:09 -0600

    Atomic 4 WIP

---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,19 @@
+// Name prompt
+var name = prompt("What's your name?");
+console.log("User name = " + name);
+alert("Hello " + name + "!");
+
+// Age prompt
+var age = prompt("How old do you happen to be?");
+console.log("User age = " + age);
+alert(name + ", you are " + age + " years old.");
+
+// Hometown prompt
+var homeTown = prompt("Where were you born?");
+console.log("User hometown = " + homeTown);
+alert("Your hometown is " + homeTown + ".");
+
+// Favorite color prompt
+var favColor = prompt("What is your favorite part of the visible light spectrum?");
+console.log("User color = " + favColor);
+alert("And your favorite color is " + favColor + "!");
