4059ba874a1bcee466c654962d39170ba4f6bf3b
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-09 14:12:34 -0600

    done with 5

--- 05_BasicMath/global.js
+++ 05_BasicMath/global.js
@@ -2,9 +2,12 @@ var input1 = prompt("Let's so some math! Please input a number.");
 var input2 = prompt("Input another number, please.");
 var input3 = prompt("One more number!");
 
-num1 = Number(input1)
-num2 = Number(input2)
-num3 = Number(input3)
+num1 = parseFloat(input1)
+console.log("typeof number 1 is " + (typeof num1));
+num2 = parseFloat(input2)
+console.log("typeof number 2 is " + (typeof num2));
+num3 = parseFloat(input3)
+console.log("typeof number 3 is " + (typeof num3));
 
 alert("CLICK OK FOR MATH!");
 alert(input1 + " + " + input2  + " + " + input3 + " = " + (num1 + num2 + num3));
