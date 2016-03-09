5515ea813c1822ae7c317ab413feade5277f2527
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-09 12:04:46 -0600

    03 done, with some interesting results

---
+++ 03_Variables/global.js
@@ -0,0 +1,11 @@
+var name = "Shannon"
+var age = 31
+var haircolor = "strawberry banana"
+//
+alert("My name is " + name+ "!");
+alert("I am " + age + " years old. (I KNOW)");
+alert("You can recognized me by my poofy " + haircolor + " hair.");
+//
+console.log("user saw your name was " + name)
+console.log("user could not believe you are " + age + " years old." )
+console.log ("user pictured cotton candy, if cotton candy were " + haircolor)
