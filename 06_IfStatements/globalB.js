d0a92acf847071a9e507eed8513666b9d438a02e
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-09 14:56:06 -0600

    Progress update

---
+++ 06_IfStatements/globalB.js
@@ -0,0 +1,9 @@
+var password = "bubblegumpopprincess"
+
+var params = prompt("Enter password:")
+
+if (params == password) {
+  alert("Good work.")
+} else {
+  alert("Sorry, the password is actually " + password)
+}
