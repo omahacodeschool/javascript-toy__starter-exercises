9a0902d5db2c3a951de4e348e1161179bb4d0600
Thorin Butler <thorin.butler@gmail.com>
2016-03-09 15:16:18 -0600

    The alerts seem to work.  Not so for the console.log()s.

---
+++ 02_OutputWithAlertAndLog/global.js
@@ -0,0 +1,13 @@
+alert("Hello! My name is Thorin.");
+
+console.log("My favorite breakfast is a fresh crepe slathered with nutella and sliced bananas.");
+
+console.log("Two cats live with us currently, though the occasional hard luck case will show up.");
+
+console.log("One of my favorite places to eat is a little greasy spoon in Santa Fe called Horseman's Haven.  Two words: fresh sopapillas");
+
+console.log("I take maybe one or two naps per week if I'm extremely lucky.");  
+
+console.log("If I had the choice, I'd probably take a nap a day.  I think a housecat snuck into my gene pool somewhere along the way.");
+
+alert("Goodbye! Thanks for visiting!");
