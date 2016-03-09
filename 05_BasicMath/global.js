c2121eefa3ed845ad1e31294c514c392cb7f55ac
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-09 13:48:29 -0600

    maths dones

--- 05_BasicMath/global.js
+++ 05_BasicMath/global.js
@@ -7,12 +7,9 @@ num2 = Number(input2)
 num3 = Number(input3)
 
 alert("CLICK OK FOR MATH!");
-alert(input1 + "+" input2 + "+" + input3 + "=");
-alert(num1 + num2 + num3);
-
-//Output the sum of the three numbers in a string like so: `7 + 4 + 12 = 23`
-//Output the difference when the second and third numbers are subtracted from the first: `7 - 4 - 12 = -9`
-//Output the product of the three numbers: `7 * 4 * 12 = 336`
-//Output the quotient of the first number divided by the second number: `7 / 4 = 1.75`
-//Output the result of incrementing the first number: `7++ = 8`
-//Output the result of decrementing the second number: `4-- = 3`
+alert(input1 + " + " + input2  + " + " + input3 + " = " + (num1 + num2 + num3));
+alert(input1 + " - " + input2  + " - " + input3 + " = " + (num1 - num2 - num3));
+alert(input1 + " * " + input2  + " * " + input3 + " = " + (num1 * num2 * num3));
+alert(input1 + " / " + input2 + " = " + (num1 / num2));
+alert(input1 + "++ = " + (++num1));
+alert(input2 + "-- = " + (--num2));
