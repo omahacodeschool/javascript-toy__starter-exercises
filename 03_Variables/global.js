d86a98646f85042175d69c5350c5e347f8ba7b30
Ryan Newell <youcatastrophe@gmail.com>
2016-03-09 13:22:01 -0600

    Completed 03 Variables

---
+++ 03_Variables/global.js
@@ -0,0 +1,9 @@
+//Variable assignment
+var name = "Bilbo Baggins";
+var age = 111 ;
+var hairColor = "sandy blonde";
+
+//Alert box statements
+alert("My name is " + " " + name + ".");
+alert("My age is " + age + ".");
+alert("My hair color is " + hairColor + ".");
