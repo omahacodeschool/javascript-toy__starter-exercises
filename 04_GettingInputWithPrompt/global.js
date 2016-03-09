d8637bd63f2e04d71d41a2e6ed6c7a3757232a40
Ryan Newell <youcatastrophe@gmail.com>
2016-03-09 15:10:29 -0600

    04 Input with Prompts finished

---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,21 @@
+//prompts and variable assignment
+var name = prompt("What is your name?");
+var age = prompt("How old are you?");
+var hometown = prompt("What is your hometown?");
+var favoriteNumber = prompt("What is your favorite number?");
+
+//alert() statement containging all answers (preferred)
+alert("Your name is " + name + ".\n You are " + age + " years old! \n You are from " + hometown + ".\n Your favorite number is " + favNumber + ".");
+
+//alert() statements for each answer
+// alert("Your name is " + name + ".");
+// alert("You are " + age + " years old!");
+// alert("You are from " + hometown + ".");
+// alert("Your favorite number is " + favNumber + ".");
+
+//console.log() statements for each answer
+console.log("Your name is " + name + ".");
+console.log("You are " + age + " years old!");
+console.log("You are from " + hometown + ".");
+console.log("Your favorite number is " + favNumber + ".");
+console.log(typeof age);
