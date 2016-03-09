a94d02e5ebbb3b33ea3aebe6e32c3b9dd0400b75
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-09 13:04:22 -0600

    04 answered

---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,12 @@
+//name, age, hometown, favorite number
+var name = prompt("What is your name?");
+var age = prompt("How old are you, " + name + "?");
+var hometown = prompt("What's your hometown?");
+var favoritenumber = prompt("What is your favorite number?");
+
+console.log("User is named " + name);
+console.log("User CLAIMS to be " + age + " years old but who can say.");
+console.log("User is from " + hometown);
+console.log("User's favorite number is " + favoritenumber + "???");
+
+alert(name + "! You were born in " + hometown + " " + age + " years ago, and your favorite number is " + favoritenumber + ". I feel like I know so much about you already!")
