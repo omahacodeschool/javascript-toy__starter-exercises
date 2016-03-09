3c33af4d77cb2846240639dcaaa7e95499d57b62
Amy Wall <amymariewall@gmail.com>
2016-03-09 11:35:27 -0600

    Second Exercise javascript complete.

--- 02_OutputWithAlertAndLog/global.js
+++ 02_OutputWithAlertAndLog/global.js
@@ -1,3 +1,12 @@
-window.onload = function Greet()  {
-  alert("Hello. My name is Banana Potato.")
-  }
+window.onload = function Greet()
+
+{
+
+alert("Hello. My name is Banana Potato.");
+
+}
+
+console.log("My favorite breakfast is bananas and potatoes.\n17 cats live with me.\nMy favorite place to eat lots of food is the grocery store.\nI take 3.7 naps per week.\nI would like to take 5.8 naps per week.");
+
+
+alert("Bye bye, friend.");
