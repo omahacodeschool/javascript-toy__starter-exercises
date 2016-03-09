e125bef77c412c04cbe2946f81d1697906f7f89c
Amy Wall <amymariewall@gmail.com>
2016-03-09 15:08:01 -0600

    Chugging away at exercises. Trying to become inspired. Finding this tedious. Just finished exercise #3.

--- 02_OutputWithAlertAndLog/global.js
+++ 02_OutputWithAlertAndLog/global.js
@@ -1,3 +1,5 @@
+//Greet the user when they get there and introduce yourself.
+
 window.onload = function Greet()
 
 {
@@ -5,8 +7,10 @@ window.onload = function Greet()
 alert("Hello. My name is Banana Potato.");
 
 }
+//List answers to life's most important questions in the JS console.
 
 console.log("My favorite breakfast is bananas and potatoes.\n17 cats live with me.\nMy favorite place to eat lots of food is the grocery store.\nI take 3.7 naps per week.\nI would like to take 5.8 naps per week.");
 
+//Say goodbye.
 
 alert("Bye bye, friend.");
---
+++ 03_Variables/global.js
@@ -0,0 +1,16 @@
+
+//Declare some variables
+
+var name = "Frank"; 
+var age; 
+var hairColor = "Red";
+
+alert("My name is " + name);
+
+setTimeout(function() {alert("My age is " + age); }, 1500);
+
+setTimeout(function() {alert("My hair color is " + hairColor); }, 1500);
+
+document.write("My age is " + age)
+console.log("My age is " + age);
+
