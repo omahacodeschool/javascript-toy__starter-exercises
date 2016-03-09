3d58896eeec8dcdbdf99ab2984918c70f6ef2368
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-09 12:20:23 -0600

    answered my own question in exercise 3

--- 03_Variables/global.js
+++ 03_Variables/global.js
@@ -1,9 +1,9 @@
-var name = 'BATMAN', 
-    age = 'UNKNOWN', 
-    hairColor = 'SHROUDED IN MYSTERY',
+var name = "BATMAN", 
+    age = "UNKNOWN", 
+    hairColor = "SHROUDED IN MYSTERY",
     test;
 
-alert('I. AM. ' + name + '.');
-alert('My age is ' + age + '.');
-alert('My hair color is ' + hairColor + '.');
-alert('this is a ' + test)
+alert(`I. AM. ${name}`);
+alert(`My age is ${age}`);
+alert(`My hair color is ${hairColor}`);
+alert(`this is a ${test}`);
