856be654c331231cca01084ebbb704f817292418
Beth Haubert <bethany.haubert@gmail.com>
2016-03-09 14:33:56 -0600

    02 finished

---
+++ 02_OutputWithAlertAndLog/global.js
@@ -0,0 +1,8 @@
+alert("Hi, I'm Clementine");
+
+console.log("My favorite breakfast is grass.");
+console.log("I live with two other furry cats and two giant hairless ones.");
+console.log("I love to eat leftover icecream on the countertop.");
+console.log("I probably take about six naps a day.");
+
+alert("Goodbye");
