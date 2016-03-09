f903b8b590fbdc5319f7728e1a3681768f2dcad0
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-09 16:36:36 -0600

    WIP counting.js

---
+++ 07_BasicLoops/counting.js
@@ -0,0 +1,16 @@
+//7B: More Counting
+
+var count = "";
+var start = prompt("Give me a starting value", "enter number");
+var end = prompt("Now give me an ending value", "enter number");
+var increment = prompt("What increment would you like to count by", "enter number");
+
+
+while (start < end) {
+    alert(count += start + ",");
+    start + increment;
+}
+
+
+
+
---
+++ 07_BasicLoops/while.js
@@ -0,0 +1,10 @@
+//JS While loop
+
+var text = "";
+i = 0;
+
+while (i < 10) {
+    alert(text += i + ",");
+    i++;
+}
+
