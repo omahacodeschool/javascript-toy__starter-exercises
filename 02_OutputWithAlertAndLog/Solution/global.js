4a733d42db7c1699ba2be02be3ddd7ab8da0bf14
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-09 11:52:15 -0600

    not working

--- 02_OutputWithAlertAndLog/Solution/global.js
+++ 02_OutputWithAlertAndLog/Solution/global.js
@@ -1,14 +1,13 @@
 // JS Atomic 02
 
 //initial greeting
-alert("Hi, my name is Jillian.");
+alert("Hi, my name is Andrew!");
 
 //console.log statements:
-console.log("My favorite breakfast is no breakfast. Eating breakfast just makes me hungry like five minutes later.");
-console.log("I live with two large, interesting cats. We have been roommates for ten years.");
-console.log("My favorite place to eat lots of food is at home. That way I know what's in it.");
-console.log("I take zero naps per week unless I'm sick.");
-console.log("I would like to continue taking zero naps per week unless I'm sick.");
+console.log("My favorite food is peanut butter chicken.");
+console.log("I live with zero cats, but in the past have lived with cats.");
+console.log("My favorite part of breakfast is breakfast.");
+console.log("My second favorite part of breakfast is making breakfast");
+console.log("I would rather be able to fly than be able to turn invisible");
 
-// Another alert() that says "Goodbye" to the user:
-alert("Peace bruh");
+alert("Now we're done with that!");
---
+++ 02_OutputWithAlertAndLog/global.js
@@ -0,0 +1,14 @@
+// JS Atomic 02
+
+//initial greeting
+alert("Hi, my name is Jillian.");
+
+//console.log statements:
+console.log("My favorite breakfast is no breakfast. Eating breakfast just makes me hungry like five minutes later.");
+console.log("I live with two large, interesting cats. We have been roommates for ten years.");
+console.log("My favorite place to eat lots of food is at home. That way I know what's in it.");
+console.log("I take zero naps per week unless I'm sick.");
+console.log("I would like to continue taking zero naps per week unless I'm sick.");
+
+// Another alert() that says "Goodbye" to the user:
+alert("Peace bruh");
