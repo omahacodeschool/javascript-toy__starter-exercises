2a0e265d770e4be4c17c477595839a624754e7e6
Tyler <tfisher322@gmail.com>
2016-03-09 11:14:15 -0600

    Almost finished with #4

---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,15 @@
+
+var name = prompt("What is your name?");
+var age = prompt("What is your age?");
+var hometown = prompt("Where is your home town?");
+var favoriteNumber = prompt("What is your favorite number?");
+
+console.log("Your name is" + " " + name + ".");
+console.log("Your age is" + " " + age + ".");
+console.log("Your hometown is" + " " + hometown + ".");
+console.log("Your favorite number is" + " " + favoriteNumber + ".");
+
+alert("Your name is" + " " + name + ".");
+alert("Your age is" + " " + age + ".");
+alert("Your hometown is" + " " + hometown + ".");
+alert("Your favorite number is" + " " + favoriteNumber + ".");
