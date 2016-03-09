5929229ca75b66a8143c9b4c1fa896307cf4635a
Michael Meier <mmeier61@yahoo.com>
2016-03-09 15:41:19 -0600

    comit

---
+++ 03_Variables/global.js
@@ -0,0 +1,6 @@
+var name = "Ben"
+var age = "35"
+var hair_color = "Black" 
+alert("My name is " + name);
+alert("I am currently" + age);
+alert("My hair color is " + color_of_hair);
---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,14 @@
+ name = prompt("Please enter your name")
+ age = prompt("What is your age?")
+ 
+ hometown = prompt("What is your home town?")
+ number = prompt("What is your favorite number?")
+
+alert("Your name is " + name)
+console.log("Your name is " + name)
+alert("Your age is " + age)
+console.log("Your age is " + age)
+alert("Your home town is " + hometown)
+console.log("Your home town is " + hometown)
+alert("Your favorite number is " + number)
+console.log("Your favorite number is " + number)
