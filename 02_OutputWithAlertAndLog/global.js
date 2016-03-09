ce053d12e592c8c9f919a5f37ea867e07ef5a226
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-09 10:38:56 -0600

    Output with alert done

---
+++ 02_OutputWithAlertAndLog/global.js
@@ -0,0 +1,15 @@
+// JS Atomic 02
+
+
+// Greeting
+alert("Hi! My name is Derek Scott Pressnall.");
+
+//console.log statements:
+console.log("I love bacon.");
+console.log("I used to have two cats but one moved down the street. I'm now left with one.");
+console.log("I love a good ol' Chinese buffet.");
+console.log("0. I'm not a nap taker. I have a hard time waking up from them. I get stuck in dream land.");
+console.log("Tons. There was once I time I loved naps and I long for that time often. If only I could let my mind rest.");
+
+
+alert("Goodbye, user!");
