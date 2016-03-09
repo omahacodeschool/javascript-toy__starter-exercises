fb1de8b2bb3f4ac15daabbd214dd787fabacce16
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-09 10:54:04 -0600

    Variables

---
+++ 03_Variables/global.js
@@ -0,0 +1,12 @@
+//Variables
+
+var name = "Scott";
+
+var age = "22";
+
+var hairColor = "red";
+
+//Console.log statements:
+console.log("My name is" + " " + name);
+console.log("My age is" + " " + age);
+console.log("My haircolor" + " " +  hairColor);
