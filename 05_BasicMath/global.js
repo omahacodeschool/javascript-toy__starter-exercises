1d5f9c7df2a961cac6415cbb8104b8f53d7edcc0
Kyle Sandage <ksandage08@gmail.com>
2016-03-09 14:20:10 -0600

    prompts not showing

--- 05_BasicMath/global.js
+++ 05_BasicMath/global.js
@@ -1,27 +1,32 @@
 // Get numbers
 var first_num = prompt("Give me a number.");
-var second_num = prompt("Give me a number.");
+first_num = parseInt(first_num);
+
+var second_num = prompt("Give me a number."); 
+second_num = parseInt(second_num);
+
 var third_num = prompt("Give me a number.");
+third_num = parseInt(third_num)
 
 // Do math
-var sum = first_num + second_num + third_num
-var differenc = first_num - second_num - third_num
-var product = first_num * second_num * third_num
-var quotient = first_num / second_num
-var increase = first_num++
-var decrease = second_num--
+var sum = (first_num + second_num + third_num);
+var differenc = (first_num - second_num - third_num);
+var product = (first_num * second_num * third_num);
+var quotient = (first_num / second_num);
+var increase = (first_num++);
+var decrease = (second_num--);
 
 // Educate user
-alert("The sum of " + first_num "," + second_num + ", and" + third_num + "is " + sum +".")
-alert("The difference of " + first_num + " from" + second_num " and" + third_num + "is " + difference +".")
-alert("The product of " + first_num + " ," + second_num + ", and" + third_num + "is " + product +".")
+alert( "The sum of " + first_num "," + second_num + ", and" + third_num + "is " sum +".");
+alert("The difference of " + first_num + " from" + second_num " and" + third_num + "is " + difference +".");
+alert("The product of " + first_num + " ," + second_num + ", and" + third_num + "is " + product +".");
 alert("The quotient of " + first_num + " divided by" + second_num +
- "is " + quotient + ".")
-alert(first_num + " increased by 1 is " + increase + ".")
-alert(first_num + " decreased by 1 is " + decrease + ".")
+ "is " + quotient + ".");
+alert(first_num + " increased by 1 is " + increase + ".");
+alert(first_num + " decreased by 1 is " + decrease + ".");
 
 // Log Stuff
-console.log(first_num)
-console.log(second_num)
-console.log(third_num)
-console.log(sum)
+console.log(first_num);
+console.log(second_num);
+console.log(third_num);
+console.log(sum);
