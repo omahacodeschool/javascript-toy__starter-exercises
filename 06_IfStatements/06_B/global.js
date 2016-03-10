9c437aee531402db4632e11239110a61858703a3
Amy Wall <amymariewall@gmail.com>
2016-03-09 18:47:11 -0600

    6B done.

---
+++ 06_IfStatements/06_B/global.js
@@ -0,0 +1,15 @@
+//6B: Password Game
+
+//In your Javascript file, hardcode a password (e.g. `var password = "monkeybrains";` but pick your own password) Prompt the user for a password; if it matches your stored password, give them an `alert()` that says "Good job!" If it doesn't match, give them an `alert()` that says something to the effect of `"Sorry, the password is actually monkeybrains."` Make sure to use the variable storing the password instead of the string-literal that represents the password text so your program can easily change.
+
+var password = "password"
+
+var userPassword = prompt("Please enter a password");
+
+if (userPassword == password) {
+    alert("Great job!");
+  }
+
+if (userPassword != password) {
+    alert("NOPE");
+  }
