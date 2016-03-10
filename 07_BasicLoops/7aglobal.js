095e89406a850ac17b2eaa142e1dca675df42594
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-09 21:46:12 -0600

    countby is now a for loop

--- 07_BasicLoops/7aglobal.js
+++ 07_BasicLoops/7aglobal.js
@@ -1,18 +1,27 @@
 var startnumber = prompt("Counting! Where should I start?")
-var i = parseInt(startnumber)
+var inumber = parseInt(startnumber)
 var endnumber = prompt("Where should I end?")
 var number = parseInt(endnumber)
 var bynumber = prompt("By what increments should i count?")
 var countby = parseInt(bynumber)
-
 if (i <= number) {
-  while (i <= number) {
-      console.log(i);
-      (i += countby);
-  }
-}  else {
-  while (i >= number) {
-      console.log(i);
-      (i -= countby);
+  for (i = inumber; i <= number; i += countby) {
+    console.log(i)
+  } 
+} else {
+  for (i = inumber; i >= number; i -= countby) {
+    console.log(i)
   }
 }
+
+//if (i <= number) {
+//  while (i <= number) {
+//      console.log(i);
+//      (i += countby);
+//  }
+//}  else {
+//  while (i >= number) {
+//      console.log(i);
+//      (i -= countby);
+//  }
+//}
