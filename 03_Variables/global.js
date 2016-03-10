2cb9697df43d98f161baf2c4747d24f11bdd9ebf
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-09 18:22:56 -0600

    JS Atomic 03

---
+++ 03_Variables/global.js
@@ -0,0 +1,10 @@
+var name = "Brad"
+var age = 39
+var hairColor = "dark brown with streaks of light brown"
+
+alert("My name is " + name + ".");
+console.log("displays name");
+alert("My age is " + age + ".");
+console.log("displays age");
+alert("My hair color is " + hairColor + ".")
+console.log("displays hair color");
