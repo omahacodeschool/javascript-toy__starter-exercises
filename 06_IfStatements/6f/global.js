64d04e479657b529d69d408dc707e2eb59a50aa0
Beth Haubert <bethany.haubert@gmail.com>
2016-03-10 14:35:13 -0600

    6f

--- 06_IfStatements/6f/global.js
+++ 06_IfStatements/6f/global.js
@@ -2,9 +2,10 @@ var a = parseInt(prompt("please input the first number"));
 var b = parseInt(prompt("please input the second number"));
 var c = parseInt(prompt("please input the third number"));
 
-alert("the sum of the three numbers is: " + (a + b + c));
-alert("the difference when other numbers are subtracted from the first: " + (a - b - c));
-alert("the product of the three numbers is: " + (a * b * c));
-alert("the quotient of the first divide by the second is: " + (a/b));
-alert("the result of incrementing the first number: " + (a++));
-alert("the result of decrementing the second number: " + (b--));
+if ((a + b > c) && (a + c > b) && (c + b > a)) {
+	console.log("great triangle");
+};
+
+else {
+	console.log("sad triangle");
+};
