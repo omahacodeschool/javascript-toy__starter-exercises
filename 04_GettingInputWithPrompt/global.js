2d063638bc4d7b075d1159bffa8e8f85eee64405
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-09 18:41:10 -0600

    JS Atomic 04 tasks complete

---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,22 @@
+// Ask the user (using prompt()) for their name, then assign that value to a well-named variable.
+var name = prompt("What is your name?");
+
+// Ask the user (using prompt()) for their age, then assign that value to a well-named variable.
+var age = prompt("What is your age?");
+
+// Ask the user (using prompt()) for their hometown, then assign that value to a well-named variable.
+var hometown = prompt("What is your hometown?");
+
+// Ask the user (using prompt()) for their favorite number, then assign that value to a well-named variable.
+var favNumber = prompt("What is your favorite number?");
+
+// Output each of those values using both console.log() and alert(). You can output the value itself with no context around it, but it's a better decision to use each of those values in a sentence. For example: "Your name is Aloysius."
+
+alert("Your name is " + name + ".");
+console.log(name);
+alert("Your age is " + age + ".");
+console.log(age);
+alert("Your hometown is " + hometown + ".");
+console.log(hometown);
+alert("Your favorite number is " + favNumber + ".")
+console.log(favNumber);
