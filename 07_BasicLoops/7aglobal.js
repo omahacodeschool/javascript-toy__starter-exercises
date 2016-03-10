0e7b3582495df082c54975000bfe5b0eb57468c4
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-09 20:32:51 -0600

    count backwards and forwards by given amt

--- 07_BasicLoops/7aglobal.js
+++ 07_BasicLoops/7aglobal.js
@@ -2,15 +2,17 @@ var startnumber = prompt("Counting! Where should I start?")
 var i = parseInt(startnumber)
 var endnumber = prompt("Where should I end?")
 var number = parseInt(endnumber)
+var bynumber = prompt("By what increments should i count?")
+var countby = parseInt(bynumber)
 
 if (i <= number) {
   while (i <= number) {
       console.log(i);
-      i++;
+      (i += countby);
   }
 }  else {
   while (i >= number) {
       console.log(i);
-      i--;
+      (i -= countby);
   }
 }
