59262217421c19db132f6afb7a3fd28f1ddc1288
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-10 17:30:23 -0600

    4 done

---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,21 @@
+//1. Ask the user (using `prompt()`) for their name, then assign that value to a well-named variable.
+
+var name = prompt("What is your name?","My name is");
+
+//2. Ask the user (using `prompt()`) for their age, then assign that value to a well-named variable.
+
+var age = prompt("What is your age?", "My age is");
+
+//3. Ask the user (using `prompt()`) for their hometown, then assign that value to a well-named variable.
+
+var hometown = prompt("Where did you grow up?", "I grew up in")
+
+//4. Ask the user (using `prompt()`) for their favorite number, then assign that value to a well-named variable.
+
+var num = prompt("What is your favorite number?", "My favorite number is")
+
+//5. Output each of those values using both `console.log()` and `alert()`. You can output the value itself with no context around it, but it's a better decision to use each of those values in a sentence. For example: "Your name is Aloysius."
+
+alert("Your name is " + name + ". You are " + age + "years old and grew up in " + hometown + ". Your favorite number is " + num)
+
+console.log("Your name is " + name + ". You are " + age + "years old and grew up in " + hometown + ". Your favorite number is " + num)
