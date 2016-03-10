e18b97abda6058ce9eb922bab8bcffe7dfdfd75d
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-09 23:15:00 -0600

    Slow progress I've been feeling ill most of the day so this is about all ive been able to manage so far tonight.  Will work on it more tomorrow.

---
+++ 02_OutputWithAlertAndLog/global.js
@@ -0,0 +1,11 @@
+//greeting
+alert("Hello! My name is Duchess!");
+
+//console.log answers
+console.log("My favorite breakfast is french toast.");
+console.log("I have 3 cats.");
+console.log("I love eating tons of sushi at Blues.");
+console.log("I don't normally take naps during the week.");
+console.log("I would like to take 17 billion naps per week.");
+
+alert("Goodbye!");
