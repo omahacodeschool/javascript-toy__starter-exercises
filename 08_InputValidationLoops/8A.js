e124355f733679ead5c81c6d44734f1eabc94dcf
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-10 14:39:42 -0600

    8B done with extra features

--- 08_InputValidationLoops/8A.js
+++ 08_InputValidationLoops/8A.js
@@ -1,15 +1,17 @@
 //8A: What's the Password?
 //Create a secret password (hard-code it as a string). Prompt the user for the password. Keep prompting until they correctly input the password, then congratulate them on a job well done. This is effectively a redo of Atomic JS-06B.
 
-
+// set variables for the password, the user's guess, and the counter that tracks how many times the user guesses the password incorrectly.
 var password = "meep",
     userGuess = prompt("What's the password?"),
     guessCount = 0;
 
+//while loop continues as long as the user input does not match the password. 
 while (userGuess.toLowerCase() != password) {
   ++guessCount
   alert(`Nope! Try again, sucka!`);
   userGuess = prompt("What's the password?");
+  console.log(userGuess, typeof userGuess);
 }
 
 alert(`Lucky guess. It took you ${guessCount} tries to figure it out`);
