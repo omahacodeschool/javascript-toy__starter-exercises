645802e5a0be416088645bee232b68b1374304fb
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-09 22:34:18 -0600

    Atomic 06D done

--- 06_IfStatements/globalD.js
+++ 06_IfStatements/globalD.js
@@ -1,13 +1,13 @@
 var age = parseInt(prompt("Please enter your age"))
 
+if ( age >= 16 ) { 
+    alert("You're eligible for a driver's license")
+}
 if ( age >= 25 ) {
     alert("You can rent a car")
 } 
 if ( age >= 21 ) { 
     alert("Please don't drink and drive")
-}
-if ( age >= 16 ) { 
-    alert("You're eligible for a driver's license")
 }
 if ( age >= 0 && age <= 15 ) { 
     alert("You can't drive yet, but you'll be able to in a few years")
