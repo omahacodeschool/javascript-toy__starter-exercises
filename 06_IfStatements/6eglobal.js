b9aa46902593276be28270e431603b8da5ab5573
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-09 18:40:34 -0600

    6e done

--- 06_IfStatements/6eglobal.js
+++ 06_IfStatements/6eglobal.js
@@ -1,15 +1,18 @@
 var inputnumber = prompt("What is your age?")
 var age = parseInt(inputnumber)
+var drivingage = 16
+var drinkingage = 21
+var carrentalage = 25
 
 if (age < 0) {
     alert("You haven't been born!");
-} else if ((age >= 0) && (age <= 15)) {
+} else if ((age >= 0) && (age <= (drivingage - 1))) {
     alert("You can't drive yet, but maybe someday!");
-} else if ((age >= 16) && (age <= 20)) {
+} else if ((age >= drivingage) && (age <= (drinkingage - 1))) {
     console.log("You're eligible for a driver's license");
-} else if ((age >= 21) && (age <= 24)) {
+} else if ((age >= drinkingage) && (age <= (carrentalage - 1))) {
     console.log("Please don't drink and drive");
-} else if (age >= 25) {
+} else if (age >= carrentalage) {
     console.log("You can rent a car!");
 }  else {
     alert("Sorry I don't think that's a number");
