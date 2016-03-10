98b1112958845a5910f47ed4df8d4a31e74bdac2
Beth Haubert <bethany.haubert@gmail.com>
2016-03-10 16:52:58 -0600

    7c done

---
+++ 07_BasicLoops/7b/global.js
@@ -0,0 +1,9 @@
+var num1 = 99
+while (num1 > 0) {
+ console.log(num1 + ' bottles of beer on the wall.');
+ console.log(num1 + ' bottles of beer. Take one down. Pass it around.');
+ console.log((num1 - 1) + ' bottles of beer on the wall.')
+num1 = (num1-1)
+}
+
+
