10e881a7640fab26f3464eb9ef9ac8f3980792d3
Amy Wall <amymariewall@gmail.com>
2016-03-09 18:37:28 -0600

    6A Complete

---
+++ 06_IfStatements/06_A/global.js
@@ -0,0 +1,32 @@
+//Guess the results of these conditionals:
+
+console.log( "1. 5 == '5' is " + (5 == '5') ); // true
+
+console.log( "2. 5 == 'five' is " + (5 == 'five') ); // false
+
+console.log( "3. 5 == '6' is " + (5 == '6') ); // false
+
+console.log( "4. 5 === '5' is " + (5 === '5') ); // true
+
+console.log( "5. 5 == 2 + 3 is " + (5 == 2 + 3) ); // false
+
+console.log( "6. 5 == 5 is " + (5 == 5) ); // true
+
+console.log( "7. true == 'true' is " + (true == 'true') ); // false
+
+console.log( "8. true === 'true' is " + (true === 'true') ); // false
+
+console.log( "9. true == false is " + (true == false) ); // false
+
+console.log( "10. 5 == 12 is " + (5 == 12) ); // false
+
+console.log( "11. 5 != 12 is " + (5 != 12) ); // true
+
+console.log( "12. 5 < 12 is " + (5 < 12) ); // true
+
+console.log( "13. 5 <= 12 is " + (5 <= 12) ); // true
+
+console.log( "14. 5 > 12 is " + (5 > 12)); // false
+
+console.log( "15. 5 >= 12 is " + (5 >= 12)); // false
+
