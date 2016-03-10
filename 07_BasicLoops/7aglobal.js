676f9e2ef7dbd4b959df88c6e0fb04fffcdf98b8
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-09 20:02:42 -0600

    input of start/end numbers. counts backwards and forwards

--- 07_BasicLoops/7aglobal.js
+++ 07_BasicLoops/7aglobal.js
@@ -2,7 +2,15 @@ var startnumber = prompt("Counting! Where should I start?")
 var i = parseInt(startnumber)
 var endnumber = prompt("Where should I end?")
 var number = parseInt(endnumber)
-while (i <= number) {
-    console.log(i);
-    i++;
+
+if (i <= number) {
+  while (i <= number) {
+      console.log(i);
+      i++;
+  }
+}  else {
+  while (i >= number) {
+      console.log(i);
+      i--;
+  }
 }
