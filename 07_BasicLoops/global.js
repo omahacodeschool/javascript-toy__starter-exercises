9de22d0ac36900b921bebea914f8c1a5a0a7764d
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-10 00:31:30 -0600

    7c done

--- 07_BasicLoops/global.js
+++ 07_BasicLoops/global.js
@@ -15,10 +15,8 @@ setEnd = parseInt(prompt("Set an ending number:")),
 setUserCount = parseInt(prompt("Set a number to count by:")),
 determineDirection = false;
 
+// method to increment positively using count to get from start to end
 function highCount (start, end, count){
-  console.log(start, typeof start)
-  console.log(end, typeof end)
-  console.log(count, typeof count)
   var countArray = [start];
   while (start<= end) {
     start = start + count;
@@ -30,10 +28,8 @@ function highCount (start, end, count){
   return countArray;
 }
 
+// method to increment negatively using count to get from start to end
 function lowCount(start, end, count){
-    console.log(start, typeof start)
-  console.log(end, typeof end)
-  console.log(count, typeof count)
   var countArray = [start];
   while (start >= end) {
     start = start - count
@@ -41,16 +37,17 @@ function lowCount(start, end, count){
         countArray.push(start);
     }
   }
-  console.log(countArray, typeof countArray)
   return countArray;
 }
 
+// method to set boolean flag depending on whether setStart variable is less than or equal to the setEnd veriable OR greater than the setEnd variable
 if (setStart <= setEnd) {
   determineDirection = true;
 } else {
   determineDirection = false;
 }
 
+//method calls desired function and triggers an alert based on determinedirection variable boolean value.
 if (determineDirection == true) {
   var highResult = highCount(setStart, setEnd, setUserCount);
   alert(highResult.join("\n"));
@@ -59,3 +56,19 @@ if (determineDirection == true) {
   alert(lowResult.join("\n"));
 
 }
+//-------------------------------------------------------
+
+//7C: A Fizzy Drink
+//7A: While Loops
+var setBeer = 100,
+setBeerArray = [];
+
+while (setBeer >= 0) {
+  --setBeer
+  if (setBeer == 0) {
+    console.log("No more bottles of beer on the wall!");
+  } else if (setBeer > 0) {
+    console.log(`${setBeer} Bottles of beer on the wall. \n ${setBeer} Bottles of beer! \n Take one down, pass it around...\n`)
+  }
+}
+
