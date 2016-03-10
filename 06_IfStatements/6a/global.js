2fd87e3d97716dd8b839917a2801ebabe77e5253
Beth Haubert <bethany.haubert@gmail.com>
2016-03-10 14:08:47 -0600

    6b

--- 06_IfStatements/6a/global.js
+++ 06_IfStatements/6a/global.js
@@ -1,39 +1,21 @@
 
-No if-statements yet. 
-Instead, well do some exploration of the relational 
-and equality operators so you can see them at work. 
-Use the console (and console.log()) to work your way
- through these statements by typing them exactly as 
- you see them into your Javascript file. 
- Before you run the code, 
- predict the output of each statement 
- (like Ive done the first one for you).
-
-(Note that some of the below examples use a double-equals == and some use a triple-equals ===.)
-
 console.log( "5 == '5' is " + (5 == '5') ); // true
-console.log( "5 == 'five' is " + (5 == 'five') );
-console.log( "5 == '6' is " + (5 == '6') );
-console.log( "5 === '5' is " + (5 === '5') );
-console.log( "5 == 2 + 3 is " + (5 == 2 + 3) );
-console.log( "5 == 5 is " + (5 == 5) );
-console.log( "5 === 5 is " + (5 === 5) );
-console.log( "true == 'true' is " + (true == 'true') );
-console.log( "true === 'true' is " + (true === 'true') );
-console.log( "true == false is " + (true == false) );
-console.log( "5 == 12 is " + (5 == 12) );
-console.log( "5 != 12 is " + (5 != 12) );
-console.log( "5 < 12 is " + (5 < 12) );
-console.log( "5 <= 12 is " + (5 <= 12) );
-console.log( "5 > 12 is " + (5 > 12));
-console.log( "5 >= 12 is " + (5 >= 12));
+console.log( "5 == 'five' is " + (5 == 'five') ); //false
+console.log( "5 == '6' is " + (5 == '6') ); //false
+console.log( "5 === '5' is " + (5 === '5') ); //false
+console.log( "5 == 2 + 3 is " + (5 == 2 + 3) );//true
+console.log( "5 == 5 is " + (5 == 5) ); //true
+console.log( "5 === 5 is " + (5 === 5) ); // true
+console.log( "true == 'true' is " + (true == 'true') );//true
+console.log( "true === 'true' is " + (true === 'true') );//false
+console.log( "true == false is " + (true == false) ); //false
+console.log( "5 == 12 is " + (5 == 12) );//false
+console.log( "5 != 12 is " + (5 != 12) );//true
+console.log( "5 < 12 is " + (5 < 12) );//true
+console.log( "5 <= 12 is " + (5 <= 12) );//true
+console.log( "5 > 12 is " + (5 > 12));//false
+console.log( "5 >= 12 is " + (5 >= 12));//false
 
-In your HTML document, 
-record your answers (organized by statements) 
-and explain what the operator does. 
-Youll want to record your guess, 
-whether your guess was right or wrong, and, 
-if it was wrong, why.
 
 
 
