03497ade62f5ee3510a7602713abb7c60c3d8ac0
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-11 13:09:05 -0600

    6B tasks done

--- 06_IfStatements/global.js
+++ 06_IfStatements/global.js
@@ -1,8 +1,17 @@
-var password = "loggins"
-var userPassword = prompt("enter a password")
+var password = "loggins";
+var userPassword = prompt("enter a password");
 if (userPassword === password) {
   alert("correct")
 }
 else { 
   alert ("wrong, the password is 'loggins'")
+};
+
+var secretNumber = 23;
+var userNumber = parseInt(prompt("enter a number"));
+if (userNumber == secretNumber) {
+  alert("correct! " + userNumber + " is " + secretNumber)
 }
+else {
+  alert ("wrong! " + userNumber + " is not " + secretNumber)
+};
