4cd54e15a18ba59b2dd8c661c2c1d5e902df8b49
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-11 10:40:47 -0600

    add, subtract, and multiply

--- 05_BasicMath/global.js
+++ 05_BasicMath/global.js
@@ -1,15 +1,20 @@
 
   // Prompt the user for three numbers (make sure to parse them to integers
   var firstNum = prompt("What is your first number?");
-  firstNum = parseInt(firstNum)
+  firstNum = parseInt(firstNum);
   var secondNum = prompt("What is your second number?");
-  secondNum = parseInt(secondNum)
+  secondNum = parseInt(secondNum);
   var thirdNum = prompt("What is your third number?");
-  thirdNum = parseInt(thirdNum)
+  thirdNum = parseInt(thirdNum);
 
-  // Output the sum of the three numbers in a string like so: 7 + 4 + 12 = 23
-  alert(firstNum " + " secondNum " + " thirdNum " = " (firstNum + secondNum + thirdNum));
+  // Output the sum of the three numbers in a string.
+  // Example: 7 + 4 + 12 = 23
+  alert(firstNum + " + " + secondNum + " + " + thirdNum + " = " + (firstNum + secondNum + thirdNum))
 
-  // Output the difference when the second and third numbers are subtracted from the first: 7 - 4 - 12 = -9
-  alert(firstNum " - " secondNum " - " thirdNum " = " (firstNum - secondNum - thirdNum));
+  // Output the difference when the second and third numbers are subtracted from the first.
+  // Example: 7 - 4 - 12 = -9
+  alert(firstNum + " - " + secondNum + " - " + thirdNum + " = " + (firstNum - secondNum - thirdNum))
 
+  // Output the product of the three numbers.
+  // Example: 7 * 4 * 12 = 336
+  alert(firstNum + " * " + secondNum + " * " + thirdNum + " = " + (firstNum * secondNum * thirdNum))
