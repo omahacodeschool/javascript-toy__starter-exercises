d07066d5e78b9ef61c4a6ea00630cab8d1bbe459
Jeremy Stoll <j8r8my@gmail.com>
2016-03-10 19:46:20 -0600

    Done

--- 06_IfStatements/6d/global.js
+++ 06_IfStatements/6d/global.js
@@ -1,13 +1,15 @@
 var userInput = prompt("Please enter your age")
 
+parseInt(userInput);
+
 var userAge = Number(userInput)
-debugger;
+
 function givePermissions() {
-debugger;
-  if (userAge.isNaN()){
+
+  if (isNaN(userAge)){
     alert("Sorry, but that's not a number");
   }
-debugger;
+
   if (userAge >= 25){
     alert("You can rent a car!");
   }
@@ -31,10 +33,3 @@ debugger;
 }
 
 givePermissions()
-
-// - **Age 25 and older**: "You can rent a car"
-// - **Age 21 and older**: "Please don't drink and drive"
-// - **Age 16 and older**: "You're eligible for a driver's license"
-// - **Age 0 - 15**: "You can't drive yet, but you'll be able to in a few years"
-// - **Any negative number**: "You haven't been born yet"
-// - **NaN**: "Sorry, but that's not a number"
