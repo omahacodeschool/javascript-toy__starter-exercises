8c64d1b80b56b68b48cb24170f79b6f566682d2e
Ryan Newell <youcatastrophe@gmail.com>
2016-03-11 10:57:33 -0600

    Finished with code for 6D part 1 and 2. Questions later.

--- 06_IfStatements/global-d.js
+++ 06_IfStatements/global-d.js
@@ -1,19 +1,15 @@
-// 6D: Driver's Ed, Part 1
+// ## 6E: Driver's Ed, Part 2
 
-// Prompt the user for their age and store it in a variable, then display any and all applicable messages below:
+// This exercise is pretty similar to Part 1, but in this case you'll *only display one message* depending on the range your user's age falls into:
 
 // - **Age 25 and older**: "You can rent a car"
-// - **Age 21 and older**: "Please don't drink and drive"
-// - **Age 16 and older**: "You're eligible for a driver's license"
+// - **Age 21 - 24**: "Please don't drink and drive"
+// - **Age 16 - 20**: "You're eligible for a driver's license"
 // - **Age 0 - 15**: "You can't drive yet, but you'll be able to in a few years"
 // - **Any negative number**: "You haven't been born yet"
 // - **NaN**: "Sorry, but that's not a number"
 
-// For example, if I am 27, I should get the messages for 16, 21, and 25. If I am 23, I should get the messages for 16 and 21, but not 25, 0, or negative.
-
-// These messages can be displayed as alerts or as logs to the console.
-
-// You will not need to use `else` or `else if` in this exercise.
+// All of your program logic (i.e. the part that decides which output to display) should be contained in a single `if` / `else if` / `else` block, rather than in separate `if` statements.
 
 var age = prompt("Please provide your age");
 age = parseInt(age);
@@ -31,31 +27,20 @@ var notBornYetMessage = "You haven't been born yet";
 var NaNMessage = "Sorry, but that's not a number";
 
 if (age >= rentCarAge){
-  alert(driveOnlyMessage);
-  alert(drinkAndDriveMessage);
   alert(rentCarMessage);
-}
-
-if (age >= drinkAndDrive && age < rentCarAge) {
-  alert(driveOnlyMessage);
+} 
+else if (age >= drinkAndDrive && age < rentCarAge) {
   alert(drinkAndDriveMessage);
-}
-
-if (age >= driveOnly && age < drinkAndDrive) {
+} 
+else if (age >= driveOnly && age < drinkAndDrive) {
   alert(driveOnlyMessage);
-}
-
-if (age <= tooYoung && age > 0) {
+} 
+else if (age <= tooYoung && age > 0) {
   alert(tooYoungMessage);
 }
-
-if (age <= 0) {
-  alert(notBornYetMessage);
-}
-
-if (isNaN(age)){
+else if (isNaN(age)){
   alert(NaNMessage);
 }
-
-console.log(age)
-
+else {
+  alert(notBornYetMessage);
+}
