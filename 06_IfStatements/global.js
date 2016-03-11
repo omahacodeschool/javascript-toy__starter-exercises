05d0bbd29215cfde8d9503d2b7135803a960775c
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-11 12:49:10 -0600

    6B: Password Game done

---
+++ 06_IfStatements/global.js
@@ -0,0 +1,8 @@
+var password = "loggins"
+var userPassword = prompt("enter a password")
+if (userPassword === password) {
+  alert("correct")
+}
+else { 
+  alert ("wrong, the password is 'loggins'")
+}
