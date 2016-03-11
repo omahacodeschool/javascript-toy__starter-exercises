d26ac73561dcdbd3d4ffbab05ce7f9f25ea8f2ce
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-11 16:37:15 -0600

    7D - done, could probably refactor

--- 07_BasicLoops/global.js
+++ 07_BasicLoops/global.js
@@ -1,19 +1,44 @@
-// Implement the 99 Bottles of Beer on the Wall song using a loop. Make sure to account for having 1 bottle (instead of "1 bottles"). Use a while loop.
+// Implement FizzBuzz. This program counts numbers as normal, except that when the number is divisible by 3, you output "Fizz", when it's divisible by 5, you output "Buzz", and when it's divisible by both 3 and 5 (e.g. 15, 30, etc.), output "FizzBuzz". 
 
-var i = 99;
+// Extend this by prompting the user for a start value and an end value. Use a while loop.
 
-while (i > 2) {
-  console.log(i + " Bottles of beer on the wall...")
-  i--;
-  console.log("Take one down... " + i + " Bottles of beer on the wall.")
-};
-while (i > 1) {
-  console.log(i + " Bottles of beer on the wall...")
-  i--;
-  console.log("Take one down... " + i + " Bottle of beer on the wall.")
-};
-while (i > 0 ) {
-  console.log(i + " Bottle of beer on the wall...")
-  i--;
-  console.log("Take it down... " + i + " Bottles of beer on the wall!")
+var numA = parseInt(prompt("Enter your first number:"));
+var numB = parseInt(prompt("Enter your last number:"));
+
+if (numA > numB) {
+  while (numA >= numB) {
+    if (numA % 15 == 0) {
+      console.log("FizzBuzz")
+    }
+    else if (numA % 5 == 0) {
+      console.log("Buzz")
+    }
+    else if (numA % 3 == 0) {
+      console.log("Fizz")
+    }
+    else {
+      console.log(numA)
+    };
+    numA--;
+  }
+}
+else if (numA < numB) {
+  while (numA <= numB) {
+    if (numA % 15 == 0) {
+      console.log("FizzBuzz")
+    }
+    else if (numA % 5 == 0) {
+      console.log("Buzz")
+    }
+    else if (numA % 3 == 0) {
+      console.log("Fizz")
+    }
+    else {
+      console.log(numA)
+    };
+    numA++;
+  }
+}
+else {
+  console.log("try again")
 };
