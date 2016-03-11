e0a8ad915bc22f5fbc318a13ed9a7a3828a15367
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-11 13:59:51 -0600

    5 half way working

---
+++ 05_BasicMath/global.js
@@ -0,0 +1,21 @@
+var first_num = prompt("Pick a first number","I pick");
+var num_1 = parseInt(first_num)
+
+console.log(num_1)
+
+var second_num = prompt("Pick a second number","I pick");
+var num_2 = parseInt(second_num)
+
+console.log(num_2)
+
+var third_num = prompt("Pick a third number","I pick");
+var num_3 = parseInt(third_num)
+
+console.log(num_3)
+
+alert(first_num + " + " + second_num + " + " + third_num + " = " + (num_1 + num_2 + num_3));
+
+alert(first_num + " - " + second_num + " - " + third_num + " = " + (num_1 - num_2 - num_3));
+
+alert(first_num + " * " + second_num + " * " + third_num + " = " + (num_1 * num_2 * num_3))
+ 
