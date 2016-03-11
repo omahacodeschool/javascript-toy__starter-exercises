8054c1f05f21ce9d1e8eb2cf0c29b45b5f5b783f
Ryan Newell <youcatastrophe@gmail.com>
2016-03-11 11:42:08 -0600

    Done with code for 7A. Questions later.

---
+++ 07_BasicLoops/global-a.js
@@ -0,0 +1,9 @@
+//  7A: While Loops
+
+// Use a while loop to count from 1-10 (do your output either using `console.log()` or `alert()`).
+
+var i = 1;
+while (i <= 10) {
+    console.log("The number is " + i);
+    i++;
+}
