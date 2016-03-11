9b2096a92136a2162464b6bc3e2f76596219ef03
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-11 13:48:39 -0600

    6D

--- 06_IfStatements/global.js
+++ 06_IfStatements/global.js
@@ -15,3 +15,24 @@ if (userNumber === secretNumber) {
 else {
   alert ("wrong! " + userNumber + " is not " + secretNumber)
 };
+
+var userAge = prompt("enter your age");
+
+if (parseInt(userAge) >= 25) {
+  alert("You can rent a car.")
+}
+if (parseInt(userAge) >= 21) {
+  alert("Please don't drink and drive.")
+}
+if (parseInt(userAge) >= 16) {
+  alert("You're eligible for a driver's license.")
+}
+if (parseInt(userAge) < 16) {
+  alert("You can't drive yet, but you'll be able to in a few years")
+}
+if (parseInt(userAge) < 0) {
+  alert("You haven't been born yet.")
+}
+if (userAge == NaN) {
+  alert("Sorry, that's not a number.")
+}
