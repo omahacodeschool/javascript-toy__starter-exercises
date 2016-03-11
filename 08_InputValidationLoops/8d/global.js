af9cf8ad9f85a2187af0ecfee63f0b5e9703c065
Beth Haubert <bethany.haubert@gmail.com>
2016-03-11 10:09:52 -0600

    8d done

---
+++ 08_InputValidationLoops/8d/global.js
@@ -0,0 +1,17 @@
+var a = parseInt(prompt("please input the first number"));
+var b = parseInt(prompt("please input the second number"));
+var c = parseInt(prompt("please input the third number"));
+
+
+while ((a + b < c) || (a + c < b) || (c + b < a)) {
+alert("sad triangle");
+
+var a = parseInt(prompt("please input the first number"));
+var b = parseInt(prompt("please input the second number"));
+var c = parseInt(prompt("please input the third number"));
+
+};
+
+alert("great triangle");
+
+
