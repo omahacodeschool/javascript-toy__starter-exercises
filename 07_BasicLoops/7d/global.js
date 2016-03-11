601a029b8f61e01c5538e92c28108c5fc94ce3f3
Beth Haubert <bethany.haubert@gmail.com>
2016-03-11 09:28:26 -0600

    7d done

--- 07_BasicLoops/7d/global.js
+++ 07_BasicLoops/7d/global.js
@@ -3,18 +3,18 @@ var number = parseInt(prompt("give me a number"));
 
 while (i < number) {
 
-	if (Number.isInteger(i / 3) == true) {
+	if ((Number.isInteger(i / 5) == true) && (Number.isInteger(i / 3) == true)) {
+		console.log("fizz buzz");
+		i++;
+	}
+
+	else if (Number.isInteger(i / 3) == true) {
 		console.log ("fizz");
 		i++;
 	}
 
 	else if (Number.isInteger(i / 5) == true) {
 		console.log("buzz");
-		i++;
-	}
-
-	else if ((Number.isInteger(i / 5) == true) && (if Number.isInteger(i / 3) == true)) {
-		console.log("fizz buzz");
 		i++;
 	}
 
