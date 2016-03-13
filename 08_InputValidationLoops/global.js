5843ef5fc2d2aa0d711baf78688a45e0d1027648
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-13 17:05:09 -0500

    08A password done

--- 08_InputValidationLoops/global.js
+++ 08_InputValidationLoops/global.js
@@ -2,20 +2,10 @@
 // Prompt the user for the password. 
 // Keep prompting until they correctly input the password, then congratulate them on a job well done.
 
-var password = "Loggins"
-var userPassword = prompt("Enter your password.")
+var password = "Loggins";
+var userPassword = prompt("Enter your password.");
 
-
-
-
-
-
-
-// +var password = "loggins"
-//  +var userPassword = prompt("enter a password")
-//  +if (userPassword === password) {
-//  +  alert("correct")
-//  +}
-//  +else { 
-//  +  alert ("wrong, the password is 'loggins'")
-//  +} 
+while(userPassword != password){
+  var userPassword = prompt("That's not correct, try again.")
+};
+alert("That's correct, please proceed.");
