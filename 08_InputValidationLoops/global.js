0f5fa651354d58b70f120ec0d58ad1bf0cda54ad
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-13 16:16:03 -0500

    08A progress

---
+++ 08_InputValidationLoops/global.js
@@ -0,0 +1,21 @@
+// Create a secret password (hard-code it as a string). 
+// Prompt the user for the password. 
+// Keep prompting until they correctly input the password, then congratulate them on a job well done.
+
+var password = "Loggins"
+var userPassword = prompt("Enter your password.")
+
+
+
+
+
+
+
+// +var password = "loggins"
+//  +var userPassword = prompt("enter a password")
+//  +if (userPassword === password) {
+//  +  alert("correct")
+//  +}
+//  +else { 
+//  +  alert ("wrong, the password is 'loggins'")
+//  +} 
