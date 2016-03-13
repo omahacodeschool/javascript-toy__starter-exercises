8a928fa5a0ddbf17d8ea4f10cfe861d7b2f85ae0
Chris Foral <chris.foral@gmail.com>
2016-03-13 17:12:08 -0500

    Exercise 07 B, user defined increment, done

--- 07_BasicLoops/B_global.js
+++ 07_BasicLoops/B_global.js
@@ -4,26 +4,28 @@ for (var i = 0; i < 2; i++) {
   inputArr.push(parseInt(prompt("Give me a number, please.")));
 }
 
+var counter = parseInt(prompt("And what would you like to count by?"));
+
 if (inputArr[0] === inputArr[1]) {
   alert("I can't count! These numbers are equal!");
 } else if (inputArr[0] < inputArr[1]) {
   var x = inputArr[0]
   var y = inputArr[1]
-  console.log("Count up from " + x + " to " + y)
+  console.log("Count up from " + x + " to " + y + " by " + counter)
 } else {
   var a = inputArr[0]
   var b = inputArr[1]
-  console.log("Count down from " + a + " to " + b)
+  console.log("Count down from " + a + " to " + b + " by " + counter)
 };
 
 while (x <= y) {
-  alert("Let's count to " + y + "! Count: " + x);
+  alert("Let's count up to " + y + " by " + counter + "! Count: " + x);
   console.log("Count: " + x);
-  x++;
+  x = x + counter;
 }
 
 while (a >= b) {
-  alert("Let's count to " + b + "! Count: " + a);
+  alert("Let's count down to " + b + " by " + counter + "! Count: " + a);
   console.log("Count: " + a);
-  a--;
+  a = a - counter;
 }
