119bfb4b0ecf2915da8c49f763e1694cd1d7c43e
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-13 20:31:58 -0500

    loops working

---
+++ 07_BasicLoops/global.js
@@ -0,0 +1,30 @@
+x = 0
+while (x <= 10){
+  alert("The current number is " + x);
+  x++;
+}
+
+var starting_num = prompt("Pick as starting number", "I pick...");
+var starting_num = parseInt(starting_num);
+
+var ending_num = prompt("Pick an end number", "I pick...");
+var ending_num = parseInt(ending_num);
+
+var increment_num = prompt("Pick a number to increment by", "I pick...");
+var increment_num = parseInt(increment_num);
+
+i = starting_num;
+if (starting_num <= ending_num){
+  while (i <= ending_num){
+    alert("The incremented number is now " + i);
+    i += increment_num;
+  }
+} else {
+  while (i >= ending_num){
+    alert("The decremented number is now " + i);
+    i -= increment_num;
+  }
+}
+
+  
+
